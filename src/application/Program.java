03182674f0a1b36e8b219fc5d98505d9e88aad1b
Joana Silva <joana.joanasantossilva.silva@gmail.com>
2022-12-12 19:45:16 +0000

    deleteById implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,8 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("=== Teste 1: seller find by id ===");
@@ -36,11 +39,19 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new ID: " + newSeller.getId());
 		
-		System.out.println("\n=== Teste 45 seller update ===");
+		System.out.println("\n=== Teste 5: seller update ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Marta Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update completed.");
+		
+		System.out.println("\n=== Teste 6: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed.");
+		
+		sc.close();
 		
 	}
 
